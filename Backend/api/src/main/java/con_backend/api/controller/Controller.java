28b1b22ede8962759b58e1699fab178238d3ec0c
Pedro <plbc@ua.pt>
2022-11-24 12:54:31 +0000

    Api update

--- Backend/api/src/main/java/con_backend/api/controller/Controller.java
+++ Backend/api/src/main/java/con_backend/api/controller/Controller.java
@@ -3,6 +3,7 @@ package con_backend.api.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 import javax.validation.Valid;
 
@@ -17,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import con_backend.api.model.Device;
 //import con_backend.api.exception.ResourceNotFoundException;
 import con_backend.api.model.User;
 import con_backend.api.repository.UserRepository;
+import con_backend.api.repository.DeviceRepository;
 import con_backend.api.exception.*;
 
 @RestController
@@ -29,16 +32,73 @@ public class Controller {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> getUserByPassword(@PathVariable(value = "username") String userUsername)
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @GetMapping("/users?username={username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable(value = "username") String userUsername)
         throws ResourceNotFoundException {
-        User user = UserRepository.findByUsername(userUsernameId)
-          .orElseThrow(() -> new ResourceNotFoundException("Employee not found for thiusernameid :: " userUsernameId));
-        return ResponseEntity.ok().boduseree);
+            User user = userRepository.findByUsername(userUsername);
+            return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId)
+        throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+          .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+        return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/users")
     public User createUser(@Valid @RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId)
+         throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+       .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+
+
+    @GetMapping("/devices?name={name}")
+    public ResponseEntity<Device> getDeviceByName(@PathVariable(value = "name") String name)
+        throws ResourceNotFoundException {
+            Device device = deviceRepository.findByName(name);
+            return ResponseEntity.ok().body(device);
+    }
+
+
+    @GetMapping("/devices/{id}")
+    public ResponseEntity<Device> getDeviceById(@PathVariable(value = "id") Long deviceId)
+        throws ResourceNotFoundException {
+        Device device = deviceRepository.findById(deviceId)
+          .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + deviceId));
+        return ResponseEntity.ok().body(device);
+    }
+
+    @PostMapping("/devices")
+    public Device createDevice(@Valid @RequestBody Device device) {
+        return deviceRepository.save(device);
+    }
+
+    @DeleteMapping("/diveces/{id}")
+    public Map<String, Boolean> deleteDivece(@PathVariable(value = "id") Long deviceId)
+         throws ResourceNotFoundException {
+        Device device = deviceRepository.findById(deviceId)
+       .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + deviceId));
+
+        deviceRepository.delete(device);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
